19aa81e1c3f2caeca3d89c6deda5abf40a138dca
FacuForsyth <framirezforsyth@gmail.com>
2022-06-22 20:13:33 -0300

    routes videogames

---
+++ api/src/controllers/generos.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const apiKey = '8e6a0d3d6edc487097d8fcc85b9e6b10';
+const {Genero} = require("../db.js")
+
+//router.get('/generos, async (req, res) => {  res.send(generosBD)})
+const getGeneroApi= async ()=> {
+    try {
+        let generos = (await axios.get(`https://api.rawg.io/api/genres?key=${apiKey}`)).data.results.map(e=> e.name);
+        //console.log(generos) //[sport , card , car]
+        generos.map(e=> {
+            Genero.findOrCreate({
+                where: {
+                    name: e.toLowerCase()
+                }
+            })
+        })
+        const generosDB = await Genero.findAll();
+        console.log("guardado en DB");
+        return generosDB;
+        
+    }
+    catch(e){
+        return(e)     //para usar el middleware armado en app
+    }
+};
+
+module.exports = {getGeneroApi}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,7 +4,7 @@ const { Router } = require('express');
 const axios = require('axios');
 const apiKey = '8e6a0d3d6edc487097d8fcc85b9e6b10'
 
-const {Genero, Videogame} = require('../db')
+const { Genero, Videogame } = require('../db')
 
 const router = Router();
 
@@ -12,7 +12,7 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 
 //TRAIGO GAMES DE LA API
-const getAPI= async ()=> {
+const getAPI = async () => {
     const videogames = await axios.get(`https://api.rawg.io/api/games?key=${apiKey}`);
 
     const info = await videogames.data.results.map(i => {
@@ -23,18 +23,18 @@ const getAPI= async ()=> {
             rating: i.rating,
 
             image: i.ibackground_image,
-            descripcion: i.descripcion,    
-            
+            descripcion: i.descripcion,
+
             //map de generos y de plataformas , xq devuelve un arreglo
-            generes: i.genres.map(i => i.name),  //array con los generos string
-            //plataforms: i.plataforms.map(i => i), 
+            genres: i.genres.map(i => i.name),  //array con los generos string
+            platforms: i.platforms.map(p => p.platform.name) //array con los generos string
         }
     })
     return info;
 };
 
 //TRAIGO DE LA BD
-const getDB = async ()=> {
+const getDB = async () => {
     return await Videogame.findAll({
         include: {
             model: Genero,
@@ -48,28 +48,54 @@ const getDB = async ()=> {
 };
 
 //concatenar lo de la api y la bd
-const getVideogames = async ()=> {
+const getVideogames = async () => {
     const infoApi = await getAPI();
     const infoDB = await getDB();
     const informacion = infoApi.concat(infoDB);
     return informacion;
 };
 
-router.get('/', async(req,res)=> {      // O ES /VIDEOGAME S ??
+router.get('/videogames', async (req, res) => {
     const name = req.query.name     //buscar que exista un quere name en la url
     let videogamesT = await getVideogames();
     //let videogamesT = await getAPI();
-    if(name){
+    if (name) {
         //filtro y veo si ese name que me pasan por query esta incluido
         //toLowerCase() para pasarlo a minuscula , asi compara los 2 valores en minuscula
         let videogameName = await videogamesT.filter(i => i.name.toLowerCase().includes(name.toLowerCase()));
 
-        videogameName.length ? res.status(200).send(videogameName) : 
-        res.status(404).send('No se encuentra el VideoJuego');
+        videogameName.length ? res.status(200).send(videogameName) :
+            res.status(404).send('No se encuentra el VideoJuego');
     }
     else {
         res.status(200).send(videogamesT);
     }
 });
+
+router.post('/videogames', async (req, res) => {
+    let { name, descripcion, relased, rating, image, platforms, genres } = req.body;
+
+    platforms = platforms?.join(', ')
+
+    try {
+        const videogameCreated = await Videogame.findOrCreate({
+            where: { 
+                name,
+                descripcion, 
+                relased, 
+                rating,
+                image, 
+                platforms 
+            }
+        })
+        await videogameCreated[0].setGenero(genres)
+    }
+    catch (e) {
+        console.log(e);
+    }
+    res.send('video juego creado correctamente')
+})
+
+//RUTA GENERO
 
 module.exports = router;
